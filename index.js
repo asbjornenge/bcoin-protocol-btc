134141a84d75fba9323a7f21aff8da8ec64b0c73
Asbjørn Enge <asbjorn@hanafjedle.net>
2018-02-10 12:09:46 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+/*!
+ * protocol/index.js - protocol constants for bcoin
+ * Copyright (c) 2014-2017, Christopher Jeffrey (MIT License).
+ * https://github.com/bcoin-org/bcoin
+ */
+
+'use strict';
+
+/**
+ * @module protocol
+ */
+
+exports.consensus = require('./lib/consensus');
+exports.errors = require('./lib/errors');
+exports.Network = require('./lib/network');
+exports.networks = require('./lib/networks');
+exports.policy = require('./lib/policy');
+exports.timedata = require('./lib/timedata');
